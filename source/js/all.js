683fa0072ae8838bfb6f9d89b22b056151291870
SimonLIN <a97210230@gmail.com>
2017-10-31 22:27:41 +0900

    add some animate

--- source/js/all.js
+++ source/js/all.js
@@ -40,11 +40,32 @@ $(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 300;
+            bottom_of_window = bottom_of_window + 200;
 
             if( bottom_of_window > bottom_of_object ){
 
                 $(this).addClass('push-up');
+            }
+        });
+
+    });
+});
+
+
+$(function() {
+    $(window).scroll( function(){
+
+        $('.fadeInDelay').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+            bottom_of_window = bottom_of_window + 200;
+
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).addClass('push-up-delay');
             }
         });
 
