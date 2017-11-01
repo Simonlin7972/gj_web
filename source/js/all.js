85a7636bbee0bdac53529b19103b9649be1f58ea
SimonLIN <a97210230@gmail.com>
2017-11-01 09:14:50 +0900

    modal desktop 80%

--- source/js/all.js
+++ source/js/all.js
@@ -1,40 +1,49 @@
+jQuery(document).ready(function($) {
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 200,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 900,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
 
-jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 200,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 900,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+  //hide or show the "back to top" link
+  $(window).scroll(function() {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if ($(this).scrollTop() > offset_opacity) {
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) {
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
-
-// slider
+  // slider popup
   $('#btn-know-more').click(function(event) {
     event.preventDefault();
-        $('.full-bg').fadeToggle(200);
-      });
+    $('.full-bg').fadeToggle(200);
+  });
 
-      $('.menu-btn').click(function(event) {
-          $('.menu-full').fadeToggle(200);
-      });
+  $('.menu-btn').click(function(event) {
+    $('.menu-full').fadeToggle(200);
+  });
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
+  // sliding
+  $('#dot-1').click(function(event) {
+    $('.slide-img-wrap').addClass('sliding').removeClass('sliding-2').removeClass('sliding-3');
+  });
+  $('#dot-2').click(function(event) {
+    $('.slide-img-wrap').addClass('sliding-2').removeClass('sliding').removeClass('sliding-3');;
+  });
+  $('#dot-3').click(function(event) {
+    $('.slide-img-wrap').addClass('sliding-3').removeClass('sliding').removeClass('sliding-2');;
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0,
+    }, scroll_top_duration);
+  });
 
 });
 
@@ -42,43 +51,43 @@ jQuery(document).ready(function($){
 // fadeIn effect
 
 $(function() {
-    $(window).scroll( function(){
+  $(window).scroll(function() {
 
-        $('.fadeInBlock').each( function(i){
+    $('.fadeInBlock').each(function(i) {
 
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 200;
+      /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+      bottom_of_window = bottom_of_window + 200;
 
-            if( bottom_of_window > bottom_of_object ){
+      if (bottom_of_window > bottom_of_object) {
 
-                $(this).addClass('push-up');
-            }
-        });
-
+        $(this).addClass('push-up');
+      }
     });
+
+  });
 });
 
 
 $(function() {
 
-    $(window).scroll( function(){
+  $(window).scroll(function() {
 
-        $('.fadeInDelay').each( function(i){
+    $('.fadeInDelay').each(function(i) {
 
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 200;
+      /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+      bottom_of_window = bottom_of_window + 200;
 
-            if( bottom_of_window > bottom_of_object ){
+      if (bottom_of_window > bottom_of_object) {
 
-                $(this).addClass('push-up-delay');
-            }
-        });
-
+        $(this).addClass('push-up-delay');
+      }
     });
+
+  });
 });
