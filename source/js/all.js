6f55fdfa5a8f38d724c78d29addbf67c927f3dc7
SimonLIN <a97210230@gmail.com>
2017-11-09 00:08:07 +0900

    1. fixed the some of mobile layout 2.remove the fade in effect

--- source/js/all.js
+++ source/js/all.js
@@ -68,47 +68,48 @@ jQuery(document).ready(function($) {
   });
 });
 
-// fadeIn effect
+// // fadeIn effect
+// $(function() {
+//   $(window).scroll(function() {
+//
+//     $('.fadeInBlock').each(function(i) {
+//
+//       var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//       /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+//       bottom_of_window = bottom_of_window + 200;
+//
+//       if (bottom_of_window > bottom_of_object) {
+//
+//         $(this).addClass('push-up');
+//       }
+//     });
+//
+//   });
+// });
+//
+//
+// $(function() {
+//   $(window).scroll(function() {
+//     $('.fadeInDelay').each(function(i) {
+//
+//       var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//       /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+//       bottom_of_window = bottom_of_window + 200;
+//
+//       if (bottom_of_window > bottom_of_object) {
+//
+//         $(this).addClass('push-up-delay');
+//       }
+//     });
+//
+//   });
+// });
 
-$(function() {
-  $(window).scroll(function() {
 
-    $('.fadeInBlock').each(function(i) {
-
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-      bottom_of_window = bottom_of_window + 200;
-
-      if (bottom_of_window > bottom_of_object) {
-
-        $(this).addClass('push-up');
-      }
-    });
-
-  });
-});
-
-
-$(function() {
-  $(window).scroll(function() {
-    $('.fadeInDelay').each(function(i) {
-
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-      bottom_of_window = bottom_of_window + 200;
-
-      if (bottom_of_window > bottom_of_object) {
-
-        $(this).addClass('push-up-delay');
-      }
-    });
-
-  });
-});
 
 // faq page
 $(function(){
