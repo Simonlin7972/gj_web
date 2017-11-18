cc23a63748f955923d6bbd73b38cd35e04c2038f
SimonLIN <a97210230@gmail.com>
2017-11-18 15:23:04 +0900

    dot-selected effect 70%

--- source/js/modal.js
+++ source/js/modal.js
@@ -18,6 +18,7 @@ jQuery(document).ready(function($) {
     $('.full-bg-2').fadeToggle(200);
   });
 
+
   // sliding-img-left
   $('#dot-1').click(function(event) {
     $('.slide-img-wrap').addClass('sliding').removeClass('sliding-2').removeClass('sliding-3').removeClass('sliding-left-animation');
@@ -39,4 +40,40 @@ jQuery(document).ready(function($) {
   $('#dot-6').click(function(event) {
     $('.slide-img-wrap').addClass('sliding-3').removeClass('sliding').removeClass('sliding-2').removeClass('sliding-left-animation-2');
   });
+
+
+// dot-selected effect
+  $('#dot-1').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-2').removeClass('dot-selected');
+    $('#dot-3').removeClass('dot-selected');
+  });
+  $('#dot-2').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-1').removeClass('dot-selected');
+    $('#dot-3').removeClass('dot-selected');
+  });
+  $('#dot-3').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-1').removeClass('dot-selected');
+    $('#dot-2').removeClass('dot-selected');
+  });
+
+  $('#dot-4').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-5').removeClass('dot-selected');
+    $('#dot-6').removeClass('dot-selected');
+  });
+  $('#dot-5').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-4').removeClass('dot-selected');
+    $('#dot-6').removeClass('dot-selected');
+  });
+  $('#dot-6').click(function(event) {
+    $(this).addClass('dot-selected');
+    $('#dot-4').removeClass('dot-selected');
+    $('#dot-5').removeClass('dot-selected');
+  });
+
+
 });
